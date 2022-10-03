031dd285742c1d4aafaa0f130b9f3814f9d84938
loconno5 <loconno5@tcd.ie>
2022-10-03 15:27:07 +0100

    Adding initial java file

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+	System.out.print("Enter equation: ");
+	Scanner input = new Scanner( System.in);
+	String equation = input.next();
+
+	}
+
+}
