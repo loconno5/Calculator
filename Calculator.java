a6f1c134000b162157e15e89916544175c49d05c
loconno5 <loconno5@tcd.ie>
2022-10-04 14:44:50 +0100

    initial commits

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-	System.out.print("Enter equation: ");
-	Scanner input = new Scanner( System.in);
-	String equation = input.next();
+		System.out.print("Enter equation: ");
+		Scanner input = new Scanner(System.in);
+		String equation = input.next();
+		equation.validString(input);
 
+
+	public void validString(String input)
+	{
+		for(int i=0; i<input.length();i++) {
+			System.out.println(equation.split("+"));
+		}
 	}
+	
 
+}
 }
